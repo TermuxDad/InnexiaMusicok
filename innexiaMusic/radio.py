ad6f7db2f7839879ea313e996be07b39ff2dab03
Dark Cyber <87921701+DarkCybers@users.noreply.github.com>
2021-07-30 23:51:00 +0530

    Create radio.py

---
+++ innexiaMusic/radio.py
@@ -0,0 +1,40 @@
+"""
+VC Music Player, Telegram Voice Chat Userbot
+Copyright (C) 2021  Zaute Km | TGVCSETS
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+"""
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from utils import mp, RADIO, USERNAME
+from config import Config
+from config import STREAM
+CHAT=Config.CHAT
+ADMINS=Config.ADMINS
+
+@Client.on_message(filters.command(["radio", f"radio@{USERNAME}"]) & filters.user(ADMINS) & (filters.chat(CHAT) | filters.private))
+async def radio(client, message: Message):
+    if 1 in RADIO:
+        k=await message.reply_text("Kindly stop existing Radio Stream /stopradio")
+        await mp.delete(k)
+        await mp.delete(message)
+        return
+    await mp.start_radio()
+    k=await message.reply_text(f"Started Radio: <code>{STREAM}</code>")
+    await mp.delete(k)
+    await mp.delete(message)
+
+@Client.on_message(filters.command(['stopradio', f"stopradio@{USERNAME}"]) & filters.user(ADMINS) & (filters.chat(CHAT) | filters.private))
+async def stop(_, message: Message):
+    if 0 in RADIO:
+        k=await message.reply_text("Kindly start Radio First /radio")
+        await mp.delete(k)
+        await mp.delete(message)
+        return
+    await mp.stop_radio()
+    k=await message.reply_text("Radio stream ended.")
+    await mp.delete(k)
+    await mp.delete(message)
